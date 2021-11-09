cc95c1c518516185370d64544c16c6f68a3ae6b2
Shevchnko <k.o.shevchenko@student.khai.edu>
2021-11-09 20:10:14 +0200

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,3 +1,22 @@
 # "Hangman 1-st stage"
 print("HANGMAN")
 print("The game will be available soon")
+# "Hangman 2-nd stage"
+print("HANGMAN")
+word = input("Guess the word:\n>")
+if word == "python":
+    print("You survived!")
+else:
+    print("You lost!")
+
+
+
+
+
+
+
+
+
+
+
+
