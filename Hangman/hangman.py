a21c1b65475e2a9eb895cea78b17772e45ddd26e
Shevchnko <k.o.shevchenko@student.khai.edu>
2021-12-21 18:01:24 +0200

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -17,32 +17,61 @@ import random
 #     print("You survived!")
 # else:
 #     print("You lost!")
-# "Hangman 4-th stage"
+# # "Hangman 4-th stage"
+# words = ('python', 'java', 'javascript', 'linux')
+# run_word = random.choice(words)
+# k = "_" * len(run_word)
+# guessed = False
+# tries = 1
+# word_letters = []
+# print(k)
+# while not guessed and tries > 0:
+#     guess = input("Input a letter:").lower()
+#     if len(guess) == 1:
+#         if guess in run_word:
+#             word_letters.appened(guess)
+#             word_list = list(k)
+#             indices = [i for i, letter in enumerate(run_word)if letter == guess]
+#             for index in indices:
+#                 word_list[index] = guess
+#             k = "".join(word_list)
+#             if "_" not in k:
+#                 print("The word is", run_word, "\nYou survived!")
+#                 break
+#             tries -= 1
+#             print(k)
+#         else:
+#             tries -= 1
+#             print("You lost!")
+# "Hangman 5-th stage"
 words = ('python', 'java', 'javascript', 'linux')
-run_word = random.choice(words)
-k = "_" * len(run_word)
+word = random.choice(words)
+under = "_" * len(word)
 guessed = False
 tries = 1
-word_letters = []
-print(k)
+guessed_letters = []
+print(under)
 while not guessed and tries > 0:
-    guess = input("Input a letter:").lower()
-    if len(guess) == 1:
-        if guess in run_word:
-            word_letters.appened(guess)
-            word_list = list(k)
-            indices = [i for i, letter in enumerate(run_word)if letter == guess]
+    guess = input("Input a letter: >").lower()
+    if guess in word:
+        if guess in guessed_letters:
+            tries -= 1
+            print("No improvements")
+            guessed_letters.appened(guess)
+            word_list = list(under)
+            indices = [i for i, letter in enumerate(word) if letter == guess]
             for index in indices:
                 word_list[index] = guess
-            k = "".join(word_list)
-            if "_" not in k:
-                print("The word is", run_word, "\nYou survived!")
-                break
-            tries -= 1
-            print(k)
-        else:
-            tries -= 1
-            print("You lost!")
+                under = "".join(word_list)
+                print(under)
+                if "_" not in under:
+                    guessed = True
+                    print("The word is", word, "\nYou win")
+                else:
+                    tries -= 1
+                    print("You lost")
+
+
 
 
 
