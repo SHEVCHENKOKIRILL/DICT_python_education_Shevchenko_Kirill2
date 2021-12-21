b362773b2f885f242a60ead085aee19aee15099a
Shevchnko <k.o.shevchenko@student.khai.edu>
2021-12-21 14:58:04 +0200

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,22 +1,52 @@
 import random
-# "Hangman 1-st stage"
-print("HANGMAN")
-print("The game will be available soon")
-# "Hangman 2-nd stage"
-print("HANGMAN")
-word = input("Guess the word:\n>")
-if word == "python":
-    print("You survived!")
-else:
-    print("You lost!")
-# "Hangman 3-rd stage"
-words = ('python', 'java', 'javascript', 'php')
-user_input = input("Guess the word:\n>")
-run = random.choice(words)
-if user_input == words:
-    print("You survived!")
-else:
-    print("You lost!")
+# # "Hangman 1-st stage"
+# print("HANGMAN")
+# print("The game will be available soon")
+# # "Hangman 2-nd stage"
+# print("HANGMAN")
+# word = input("Guess the word:\n>")
+# if word == "python":
+#     print("You survived!")
+# else:
+#     print("You lost!")
+# # "Hangman 3-rd stage"
+# words = ('python', 'java', 'javascript', 'php')
+# user_input = input("Guess the word:\n>")
+# run = random.choice(words)
+# if user_input == words:
+#     print("You survived!")
+# else:
+#     print("You lost!")
+# "Hangman 4-th stage"
+words = ('python', 'java', 'javascript', 'linux')
+run_word = random.choice(words)
+k = "_" * len(run_word)
+guessed = False
+tries = 1
+word_letters = []
+print(k)
+while not guessed and tries > 0:
+    guess = input("Input a letter:").lower()
+    if len(guess) == 1:
+        if guess in run_word:
+            word_letters.appened(guess)
+            word_list = list(k)
+            indices = [i for i, letter in enumerate(run_word)if letter == guess]
+            for index in indices:
+                word_list[index] = guess
+            k = "".join(word_list)
+            if "_" not in k:
+                print("The word is", run_word, "\nYou survived!")
+                break
+            tries -= 1
+            print(k)
+        else:
+            tries -= 1
+            print("You lost!")
+
+
+
+
 
 
 
