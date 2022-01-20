131c0377f12ac08c9e88a9071a2f0d714587009d
Shevchnko <k.o.shevchenko@student.khai.edu>
2022-01-20 19:29:21 +0200

    CreditCalculator 1

---
+++ CreditCalculator/CreditCalculator.py
@@ -0,0 +1,14 @@
+# CreditCalculator 1
+principal = "loan principal: 1000"
+month_1 = "Month 1: repaid 250"
+month_2 = "Month 2: repaid 250"
+month_3 = "Month 3: repaid 500"
+repaid = "the loan has been repaid!"
+
+print("principal")
+print("month_1")
+print("month_2")
+print("month_3")
+print("repaid")
+
+
