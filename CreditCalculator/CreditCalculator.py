75a1f6b6bfd41fafa8ba3e71809658d996b5c5ce
Shevchnko <k.o.shevchenko@student.khai.edu>
2022-01-20 19:29:46 +0200

    CreditCalculator 2

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -11,4 +11,30 @@ print("month_2")
 print("month_3")
 print("repaid")
 
+# CreditCalculator 2
+import math
+print("Enter the loan principal:")
+t = float(input("> "))
+print("""
+What do you want to calculate?
+type "m" – for number of monthly payments,
+type "p" – for the monthly payment:""")
+c = input("> ")
+if c == "m":
+    print("Enter the monthly payment:")
+    o = int(input("> "))
+    n = math.ceil(t / o)
+    print("It will take " + str(n) + " months to repay the loan")
+if c == "p":
+    print("Enter the number of months:")
+    n = int(input("> "))
+    o = math.ceil(t / n)
+    l = t - (n - 1) * o
+    if l == o:
+        print("Your monthly payment  " + str(o))
+    else:
+        print("Your monthly payment  " + str(o) + " and the lasy payment = " + str(l) + ".")
+else:
+    print("No correct!")
+
 
