e44d85e53908fb784f7581dac8ded8ffc12f1c23
Shevchnko <k.o.shevchenko@student.khai.edu>
2022-01-21 15:11:14 +0200

    Hangman2

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,6 +2,13 @@ import random
 # Hangman1
 print("Hangman")
 print("The game will be available soon")
+# Hangman2
+print("HANGMAN")
+word = input("Guess the word:\n>")
+if word == "python":
+    print("You survived!")
+else:
+    print("You lost!")
 
 
 
