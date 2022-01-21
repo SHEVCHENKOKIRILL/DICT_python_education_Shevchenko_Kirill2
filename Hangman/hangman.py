fcb16d3ccb1ec1b7621037954679994a5c763d21
Shevchnko <k.o.shevchenko@student.khai.edu>
2022-01-21 15:12:53 +0200

    Hangman6

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -53,6 +53,36 @@ while not g and tries > 0:
             print("That letter doesn't appear in the word")
 else:
     print("Thank for playing!\nWe'll see how well you did in the next stage")
+# Hangman6
+words = ["fire", "earth", "water", "ice"]
+word = random.choice(words)
+u = "_" * len(word)
+guessed = False
+tries = 8
+le = []
+print(u)
+while not guessed and tries > 0:
+    g = input("Input a letter: >").lower()
+    if len(g) == 1:
+        if g in word:
+            if g in le:
+                tries -= 1
+                print("No improvements")
+            le.append(g)
+            word_list = list(u)
+            indices = [i for i, letter in enumerate(word) if letter == g]
+            for index in indices:
+                word_list[index] = g
+            u = "".join(word_list)
+            print(u)
+            if "_" not in u:
+                guessed = True
+                print("The word is", word, "\nYou win")
+        else:
+            tries -= 1
+            print("That letter doesn't appear in the word")
+else:
+    print("Thanks for playing!\nWe'll see how well you did in the next stage")
 
 
 
